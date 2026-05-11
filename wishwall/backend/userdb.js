409236c667aebbfc03f084c73c24a2e8823b6da7
Aryan. <79081443+Master-Gamer-glitch@users.noreply.github.com>
2026-05-11 15:37:55 +0000

    add backend

---
+++ wishwall/backend/userdb.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true, // obviously...
+        unique: true
+    },
+    
+    name:{
+        type: String,
+        required: true,
+    },
+    
+    age: {
+        type: Number,
+        required: true
+    },
+
+    password:{
+        type: String,
+        required: true
+    },
+
+    email:{
+        type: String,
+        required: true
+    }
+
+})
+
+module.exports = mongoose.model("newUser", userSchema);
